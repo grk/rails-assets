a00fb01f1d5b28faa6260696f9522f400a1a0d32
Grzesiek Kolodziejczyk <gkolodziejczyk@gmail.com>
2013-12-20 12:58:10 +0100

    Use redis-mutex for locking instead of flock

---
+++ app/models/build/locking.rb
@@ -0,0 +1,43 @@
+module Build
+  module Locking
+    extend self
+
+    # Create an exclusive lock and yield
+    def lock(lock_name, options = {})
+      mutex = build_mutex(lock_name, options)
+
+      begin
+        mutex.lock
+        yield
+      ensure
+        mutex.unlock
+      end
+    end
+
+    # Create a lock and yield, other threads waiting for the same lock
+    # will return when the lock is done without doing any work
+    def lock_once(lock_name, options = {})
+      mutex = build_mutex(lock_name, options)
+
+      begin
+        if mutex.try_lock # make a nonblocking attempt to lock
+          # we're the first process to obtain the lock, perform
+          yield
+        else
+          # Another threat is already synchronizing, let's just lock and
+          # wait until it's done
+          mutex.lock
+        end
+      ensure
+        mutex.unlock
+      end
+    end
+
+    private
+    def build_mutex(lock_name, options = {})
+      duration = options.fetch(:duration, 240)
+      expire = options.fetch(:expire, 260)
+      Redis::Mutex.new(lock_name, block: duration, expire: expire)
+    end
+  end
+end
